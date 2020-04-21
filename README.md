2e5cd3778efeaddb7a185d8be9f3237617958f26
Matt Rudge <mrudge@gmail.com>
2020-04-21 08:41:41 +0000

    add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTML Challenge 1
+
+Your challenge is to add a HTML heading to the file.
+
+Use what you've learned in previous lessons to do this.
--- index.js
+++
@@ -1,7 +0,0 @@
-function buttonClick() {
-    document.getElementById("par").innerHTML = "<strong>You Clicked</strong>";
-}
-
-// Scripts need the next line for testing purposes
-
-module.exports = buttonClick;
